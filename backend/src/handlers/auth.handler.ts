b44c1df2654b21f2ac6319d0d3f7462d97f1bf5d
Akdev Saha <akdevsaha2003@gmail.com>
2026-01-17 22:56:34 +0530

    feat: add auth apis

--- backend/src/handlers/auth.handler.ts
+++ backend/src/handlers/auth.handler.ts
@@ -5,6 +5,7 @@ import { createToken } from "../service/createToken.js";
 import { config } from "../config/config.js";
 import { ZodError } from "zod";
 import { Prisma } from "../generated/prisma/client.js";
+import { verifyUser } from "../service/verifyUser.js";
 
 export const signUp = async (req: Request, res: Response) => {
     try {
@@ -50,10 +51,55 @@ export const signUp = async (req: Request, res: Response) => {
     }
 }
 
-export const signIn = (req: Request, res: Response) => {
+export const signIn = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = authSchema.parse(req.body)
+        const user = await verifyUser({ email, password })
+        if (!user) {
+            return res.status(400).json({ success: false, error: "cannot create user" })
+        }
+        const jwt = createToken({ id: user.id, email: user.email })
+        res.cookie("token", jwt, {
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+            secure: config.nodeEnv === "production",
+            sameSite: "strict",
+            httpOnly: true
+        })
+        return res.status(201).json({ success: true, message: "Logged in successfully" })
+    } catch (error: unknown) {
+        if (error instanceof ZodError) {
+            return res.status(400).json({
+                success: false,
+                error: "Validation failed",
+                details: error.issues
+            })
+        }
+        if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === "P2002") {
+            return res.status(409).json({
+                success: false,
+                error: "Email already exists",
+            });
+        }
 
+        if (error instanceof Error) {
+            return res.status(400).json({
+                success: false,
+                error: error.message,
+            });
+        }
+        console.error("Unknown error:", error);
+        return res.status(500).json({
+            success: false,
+            error: "Internal server error",
+        });
+    }
 }
 
 export const signOut = (req: Request, res: Response) => {
-
+    res.clearCookie("token", {
+        httpOnly: true,
+        secure: config.nodeEnv === "production",
+        sameSite: "strict"
+    })
+    res.status(200).json({ success: true, message: "Signed out successfully" })
 }
--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { signUp } from "../handlers/auth.handler.js";
+import { signIn, signOut, signUp } from "../handlers/auth.handler.js";
 const router = Router()
 
 router.post("/signup", signUp)
-
+router.post("/signin", signIn)
+router.post("/signout", signOut)
 export default router
