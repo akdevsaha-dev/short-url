aec92eab389e4f6ef7cc6f68b97e0f9fd60735fd
Akdev Saha <akdevsaha2003@gmail.com>
2026-01-23 01:15:44 +0530

    .

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { signIn, signOut, signUp } from "../handlers/auth.handler.js";
+import { rateLimiter } from "../middleware/rateLimit.js";
 const router = Router()
 
-router.post("/signup", signUp)
-router.post("/signin", signIn)
+router.post("/signup", rateLimiter, signUp)
+router.post("/signin",rateLimiter, signIn)
 router.post("/signout", signOut)
 export default router
