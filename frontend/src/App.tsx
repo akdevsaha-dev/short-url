a433d154cdc3b82918174e141d2d86c4d5a2aba5
Akdev Saha <akdevsaha2003@gmail.com>
2026-02-01 02:36:53 +0530

    feat: add authcheck

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,34 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Signup } from "./pages/Signup";
 import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import { useAuthStore } from "./store/authStore";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 function App() {
+  const checkAuth = useAuthStore((s) => s.checkAuth);
+  const isCheckingAuth = useAuthStore((s) => s.isCheckingAuth);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  if (isCheckingAuth) {
+    return (
+      <div className="h-screen w-full flex justify-center items-center">
+        <Loader className="animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div className="">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
