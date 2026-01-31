642c06968a9eb353ac78b4522e3478e0f8017baf
Akdev Saha <akdevsaha2003@gmail.com>
2026-02-01 02:34:44 +0530

    ui: made it responsive for logged in users

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,24 +1,52 @@
+import { Link, useNavigate} from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
+
 export const Navbar = () => {
+  const user = useAuthStore((state) => state.user);
+  const signout = useAuthStore((state) => state.signout);
+  const navigate = useNavigate();
+  function handleSignout() {
+    signout();
+    navigate("/");
+  }
   return (
     <nav className="fixed top-0 left-0 z-50 w-full h-16 bg-white/40 backdrop-blur-[20px]">
       <div className="relative mx-auto flex h-full max-w-7xl items-center px-6 lg:px-12">
-        <div className="shrink-0 text-2xl font-bold">
-          shrtn
-        </div>
-        <div className="absolute left-1/2 hidden -translate-x-1/2 md:flex gap-8 text-neutral-400">
-          <a className="hover:text-black cursor-pointer">Features</a>
-          <a className="hover:text-black cursor-pointer">How it works</a>
-          <a className="hover:text-black cursor-pointer">Pricing</a>
-        </div>
-        <div className="ml-auto flex items-center gap-4">
-          <button className="hidden lg:block rounded-lg px-4 py-2 text-sm hover:bg-neutral-100">
-            Log in
-          </button>
-          <button className="rounded-lg bg-black px-4 py-2 text-sm text-white">
-            Get Started
-          </button>
-        </div>
+        <div className="shrink-0 text-2xl font-bold">shrtn</div>
+        {!user ? (
+          <div className="absolute left-1/2 hidden -translate-x-1/2 md:flex gap-8 text-neutral-400">
+            <a className="hover:text-black cursor-pointer">Features</a>
+            <a className="hover:text-black cursor-pointer">How it works</a>
+            <a className="hover:text-black cursor-pointer">Pricing</a>
+          </div>
+        ) : null}
 
+        <div className="ml-auto flex items-center gap-4">
+          {!user ? (
+            <Link
+              to={"/signin"}
+              className="hidden lg:block rounded-lg px-4 py-2 text-sm hover:bg-neutral-100"
+            >
+              Log in
+            </Link>
+          ) : null}
+
+          {user ? (
+            <button
+              onClick={handleSignout}
+              className="rounded-lg bg-black px-4 py-2 text-sm text-white"
+            >
+              Log out
+            </button>
+          ) : (
+            <Link
+              to={"/signup"}
+              className="rounded-lg bg-black px-4 py-2 text-sm text-white"
+            >
+              Get started
+            </Link>
+          )}
+        </div>
       </div>
     </nav>
   );
